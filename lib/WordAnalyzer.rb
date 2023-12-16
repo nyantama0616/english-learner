02697b17821123672e0f8815a1b3fe41ee99a693
nyantama <r_nyantama0616@yahoo.co.jp>
2023-12-17 00:40:45 +0900

    品詞解析はとりあえずできた
    精度はちょっと足りないことがある

--- lib/WordAnalyzer.rb
+++ lib/WordAnalyzer.rb
@@ -1,9 +1,19 @@
 class WordAnalyzer
-  @@lemmatizer = Lemmatizer.new
+  @@tagger = EngTagger.new
+
   class << self
     # 英単語をlemmatizeする関数
     def lemmatize(word, pos = nil)
       @@lemmatizer.lemma(word, pos)
+    end
+
+    # 英文を品詞解析する関数(動詞と名詞のみ. それ以外はnilを返す)
+    @@verbs = ["VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
+    @@nouns = ["NN", "NNS", "NNP", "NNPS"]
+    @@adjs = ["JJ", "JJR", "JJS"]
+    @@advs = ["RB", "RBR", "RBS", "MD"]
+    def part_of_speechs(sentence)
+      tagged = @@tagger.get_readable(sentence.downcase).split(" ").map { |word| word.split("/") }
     end
   end
 end
