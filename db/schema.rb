eaee0394920ac23b8fed2c8cc80499fab9d687e9
nyantama <r_nyantama0616@yahoo.co.jp>
2023-12-22 14:08:32 +0900

    Word.nameのインデックスを張った
    いずれかのマイグレーションファイルで指定してるはずだけど、どれかわからん。。。

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_14_132646) do
     t.bigint "word_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["word_id"], name: "index_basic_train_data_on_word_id"
+    t.index ["word_id"], name: "index_basic_train_data_on_word_id", unique: true
   end
 
   create_table "words", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_14_132646) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "pronunciation", null: false
+    t.index ["name"], name: "index_words_on_name", unique: true
   end
 
   add_foreign_key "basic_train_data", "words"
